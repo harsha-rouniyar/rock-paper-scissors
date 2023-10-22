5daca9cc2e8acd6b3982f68a188ec14fc352d7d0
harsha-rouniyar <148663731+harsha-rouniyar@users.noreply.github.com>
2023-10-21 18:42:30 -0600

    Update r-p-s.cpp

--- r-p-s.cpp
+++ r-p-s.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main() {
+    std::cout << "Welcome to Rock, Paper, Scissors!" << std::endl;
+    
+    while (true) {
+        // User's choice
+        std::cout << "Enter your choice (1 for Rock, 2 for Paper, 3 for Scissors, 0 to quit): ";
+        int userChoice;
+        std::cin >> userChoice;
+        
+        if (userChoice == 0) {
+            std::cout << "Thanks for playing! Goodbye." << std::endl;
+            break;
+        } else if (userChoice < 1 || userChoice > 3) {
+            std::cout << "Invalid choice. Please try again." << std::endl;
+            continue;
+        }
+        
+        // Generate computer's choice
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+        int computerChoice = std::rand() % 3 + 1; // Random number between 1 and 3
+
+        // Display choices
+        const char* choices[] = {"Rock", "Paper", "Scissors"};
+        std::cout << "You chose: " << choices[userChoice - 1] << std::endl;
+        std::cout << "Computer chose: " << choices[computerChoice - 1] << std::endl;
+
+        // Determine the winner
+        if (userChoice == computerChoice) {
+            std::cout << "It's a tie!" << std::endl;
+        } else if ((userChoice == 1 && computerChoice == 3) ||
+                   (userChoice == 2 && computerChoice == 1) ||
+                   (userChoice == 3 && computerChoice == 2)) {
+            std::cout << "You win!" << std::endl;
+        } else {
+            std::cout << "Computer wins!" << std::endl;
+        }
+    }
+    
+    return 0;
+}
 
